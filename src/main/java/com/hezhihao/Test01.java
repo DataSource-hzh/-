ea8541f1be25cfa5166b7de9a1d424dd0ec68bcb
hezhihao <850569894@qq.com>
2021-06-13 13:55:07 +0800

    第二次提交代码

--- src/main/java/com/hezhihao/Test01.java
+++ src/main/java/com/hezhihao/Test01.java
@@ -3,5 +3,8 @@ package com.hezhihao;
 public class Test01 {
     public static void main(String[] args) {
         System.out.println("测试代码");
+        for (int i = 0; i < 2; i++) {
+            System.out.println(1);
+        }
     }
 }
